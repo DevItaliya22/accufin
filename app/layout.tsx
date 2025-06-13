b3fc69c0f9e01314b931c099561c39962e3239e1
Dev Italiya <devitaliya22@gmail.com>
2025-06-13 15:08:59 +0530

    final chnags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./_component/Navbar";
-import Hero from "./_component/Hero";
-import AboutCompany from "./_component/AboutCompany";
-import Company from "./_component/Company";
-import Partner from "./_component/Partner";
-import Finance from "./_component/Finance";
-import Faq from "./_component/Faq";
-import BlogSection from "./_component/BlogSection";
-import Footer from "./_component/Footer";
-import Logos from "./_component/logos";
 import { Toaster } from "react-hot-toast";
 import { Providers } from "@/lib/provider";
 
@@ -40,16 +30,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <Navbar />
-        <Hero />
-        <AboutCompany />
-        <Company />
-        <Logos />
-        <Partner />
-        <Finance />
-        <Faq />
-        <BlogSection />
-        <Footer /> */}
         <Toaster />
         <Providers>{children}</Providers>
       </body>
